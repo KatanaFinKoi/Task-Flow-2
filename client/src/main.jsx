84b8bf18cea62c3906f281c9fe3122b7927c4358
Liam Rayback <liam@macbook.local>
2025-02-06 08:41:33 -0700

    changing graphql endpoint to /graphql

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,7 +12,7 @@ import Calendar from "./components/Calendar.jsx";
 
 // Apollo Client setup
 const client = new ApolloClient({
-  uri: import.meta.env.VITE_GRAPHQL_URI,
+  uri: "/graphql" || "http://localhost:5002/graphql",
   cache: new InMemoryCache(),
 });
 
